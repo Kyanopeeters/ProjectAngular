bf15b9e3b43061841411fa02879db6b6180c1f69
Kyano Peeters <71751986+Kyanopeeters@users.noreply.github.com>
2023-11-20 16:23:06 +0100

    Add files via upload

---
+++ Angular-frontend/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true, 
+    AUTH0_DOMAIN: "dev-73fzzp1bqci7btap.us.auth0.com",
+    AUTH0_CLIENT_ID: "LqeDSwhDC6fueyZ6hn9RylXKBYDDZPZH",
+    redirectUri: "http://localhost:4200",
+    api_url: "https://localhost:6587/api",
+    AUTH0_AUDIENCE: "https://localhost:6587"
+};

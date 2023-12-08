583cb61ac026974c9d63351a072cc0fdc299c9e7
Kyanopeeters <r0832584@student.thomasmore.be>
2023-12-08 16:14:18 +0100

    kp_end

--- Angular-frontend/src/environments/environment.ts
+++ Angular-frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true, 
     AUTH0_DOMAIN: "dev-73fzzp1bqci7btap.us.auth0.com",
     AUTH0_CLIENT_ID: "LqeDSwhDC6fueyZ6hn9RylXKBYDDZPZH",
-    redirectUri: "https://project-angular-two.vercel.app/myTrips",
+    redirectUri: "https://project-angular-two.vercel.app/",
     api_url: "https://tripplannerbackendapi20231127181332.azurewebsites.net/api",
     AUTH0_AUDIENCE: "https://tripplannerbackendapi20231127181332.azurewebsites.net"
 };
